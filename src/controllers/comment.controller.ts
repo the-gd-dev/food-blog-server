87593529bd50e950186c8df85db8f7f6bea74cd3
Ghanshyam Dutt <ghanshyamdutt1407@gmail.com>
2025-05-12 12:03:37 +0530

    comments controller updated.

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -1,4 +1,4 @@
-import { INTERNAL_ERROR, SUCCESS } from "@constants";
+import { INTERNAL_ERROR, NOT_FOUND, SUCCESS } from "@constants";
 import { Comment } from "@models";
 import {
   commentSchema,
@@ -18,7 +18,7 @@ export const getAllComments = async (req: Request, res: Response) => {
     const comments = await Comment.find(
       { postId: new mongoose.Types.ObjectId(req.params.postId) },
       "-__v"
-    ).populate("userId", 'name profilePicture');
+    ).populate("userId", "name profilePicture");
     return res.status(SUCCESS.code).json({ ...SUCCESS, data: comments });
   } catch (error) {
     const validationError = formattedYupErrors(error);
@@ -47,13 +47,38 @@ export const createComment = async (req: Request, res: Response) => {
 };
 
 /**
+ * Create comment related to post
+ * @param req
+ * @param res
+ */
+export const updateComment = async (req: Request, res: Response) => {
+  try {
+    await commentSchema.validate(req.body, { abortEarly: false });
+    const comment = await Comment.findByIdAndUpdate(req.params.commentId, {
+      ...excludeKeysFromObject(req.body, ["_id", "id"]),
+      userId: (req?.user as any)?._id,
+    });
+    const data = await comment?.populate("userId", "name profilePicture");
+    return res.status(SUCCESS.code).json({ ...SUCCESS, data: data });
+  } catch (error) {
+    const validationError = formattedYupErrors(error);
+    return res.status(validationError.code).json({ ...validationError, error });
+  }
+};
+
+/**
  * Delete comment related to post
  * @param req
  * @param res
  */
 export const deleteComment = async (req: Request, res: Response) => {
   try {
-    return res.status(SUCCESS.code).json({ ...SUCCESS });
+    const findComment = Comment.findById(req?.body?.id);
+    if (!findComment) return res.status(NOT_FOUND.code).json({ ...NOT_FOUND });
+    await Comment.findByIdAndDelete(req?.body?.id);
+    return res
+      .status(SUCCESS.code)
+      .json({ ...SUCCESS, data: { id: req?.body?.id } });
   } catch (error) {
     return res.status(INTERNAL_ERROR.code).json({ ...INTERNAL_ERROR });
   }
--- src/routes/comments.routes.ts
+++ src/routes/comments.routes.ts
@@ -1,9 +1,9 @@
-import { createComment, deleteComment } from "@controllers";
+import { createComment, deleteComment, updateComment } from "@controllers";
 import { authenticateUser } from "@middlewares";
 import { Router } from "express";
 const router = Router();
 
 router.post("/create", authenticateUser, createComment);
 router.post("/delete", authenticateUser, deleteComment);
-
+router.put("/:commentId/update", authenticateUser, updateComment);
 export default router;
