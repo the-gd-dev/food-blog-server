45d6d67beb23a5854b236c20c1c997ca78ecdc57
Ghanshyam Dutt <ghanshyamdutt1407@gmail.com>
2025-05-14 20:19:44 +0530

    fixed minor things for windows sys.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import app from "./app";
 import { connectDB } from "./config/db";
 connectDB().then(() => {
-  console.log("✅ DB connected");
+  console.log("\x1b[32m✅ DB connected" + "\x1b[0m");
   app.listen(process.env.PORT, () => {
-    console.log("✅ Server is running at port: " + process.env.PORT);
+    console.log("\x1b[32m✅ Server is running at port: " + process.env.PORT + "\x1b[0m");
   });
 });
